def2f5cce61c05e6d649a0f28101b4c961fca61c
rogerwillis <roger@rogerwillis.com>
2014-08-25 20:16:37 -0700

    Added a bunch of changes...

--- js/app.js
+++ js/app.js
@@ -1,17 +1,125 @@
+$(document).ready(function() {
+//-------------------------------------------------------------
+//-------------------------------------------------------------	
+//FUNTIONS-------------------------------------------------------
+//-------------------------------------------------------------	
+//-------------------------------------------------------------
 
-$(document).ready(function(){
+
+var globalSecretNum;
+//start new game function
+var newGame = function() {
+    //generate secret number between 1 and 100
+    resetGuess();
+    resetCounter();
+	resetGuessList();	
+	resetFeedback();
+    secretNumber();
+    resetGuess();
+
+};
+
+//GENERATE RANDOM NUMBER
+var secretNumber = function() {
+    var randomNumber = Math.floor(Math.random() * 99);
+    globalSecretNum = randomNumber;
+    console.log(randomNumber);
+    return randomNumber;
+};
+
+//GUESS Functionaliyu	
+var guess = function(userInput) {
+
+    if (userInput != "") {
+	$("ul#guessList").append("<li>"+userInput+"</li>");
+        guessCounter();
+        resetGuess();
+        console.log("user Input:" + userInput + "Number is:" + globalSecretNum);
 	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+		
+		var userGuessMinusSecretNumber = Math.abs(userInput - globalSecretNum);
+		console.log(userGuessMinusSecretNumber);
+        //hot
+	
+        if (userGuessMinusSecretNumber == 0) {
+            $("#feedback").text("you got it!")
+        } else if (userGuessMinusSecretNumber <= 5) {
+            $("#feedback").text("you be getting hot!!");
+        }
+        //warm
+        else if (userGuessMinusSecretNumber <= 15) {
+             $("#feedback").text("you be gettin warmer!!");
+        }
+        //cold
+        else if (userGuessMinusSecretNumber <= 30) {
+            $("#feedback").text("you be cold!!");
+        }
+        //ice cold
+        else {
+             $("#feedback").text("Ice ice cold baby")
+        }
 
-  	});
+    }
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+
+};
+
+//The Guess Counter
+var guessCounter = function() {
+    var guessCount = parseInt($("#count").text());
+    guessCount++;
+    $("#count").text(guessCount);
+}
+
+
+//-------------------------------------------------------------	
+//RESETS-------------------------------------------------------
+//-------------------------------------------------------------	
+
+var resetCounter = function() {
+    $("#count").text(0);
+};
+
+var resetGuess = function() {
+    $("#userGuess").val("");
+};
+
+var resetGuessList = function(){
+	 $("#guessList li").remove("");
+	};
+
+var resetFeedback = function(){
+	$("#feedback").text("Make your Guess!");
+	};
+
+
+    //Start New Game on Load
+    newGame();
+
+    //Start new game on click
+    $(".new").on("click", function() {
+        newGame();
+    });
+
+    //guess
+    $("#guessButton").on("click", function(e) {
+        e.preventDefault();
+		var userGuess = $("#userGuess").val();
+        guess(userGuess);
+		
+    });
+
+    /*--- Display information modal box ---*/
+    $(".what").click(function() {
+        $(".overlay").fadeIn(1000);
+
+    });
+
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function() {
+        $(".overlay").fadeOut(1000);
+    });
+
 
 });
-
 
